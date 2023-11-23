6fc268b318e56281bdc0c342811ea4f70972d7fa
CorrectRoadH <a778917369@gmail.com>
2023-11-23 15:18:32 +0800

    feat: implemenet read config from env

--- server/plugin/ota/main.go
+++ server/plugin/ota/main.go
@@ -1,6 +1,8 @@
 package ota
 
 import (
+	"os"
+
 	"github.com/flipped-aurora/gin-vue-admin/server/plugin/ota/global"
 	"github.com/flipped-aurora/gin-vue-admin/server/plugin/ota/router"
 	"github.com/gin-gonic/gin"
@@ -10,10 +12,21 @@ type OTAPlugin struct {
 }
 
 func CreateOTAPlug() *OTAPlugin {
-	global.GlobalConfig.BuildHost = "192.168.20.233:22"
+	buildHost := os.Getenv("BuildHost")
+	if buildHost == "" {
+		buildHost = "192.168.20.233:22"
+	}
+	global.GlobalConfig.BuildHost = buildHost
+
 	global.GlobalConfig.BuildUser = "ctrdh"
 	global.GlobalConfig.BuildPassword = "223306"
-	global.GlobalConfig.BuildPath = "/home/ctrdh/CasaOS/operating-system"
+
+	buildPath := os.Getenv("BuildPath")
+	if buildPath == "" {
+		buildHost = "/home/ctrdh/CasaOS/operating-system"
+	}
+	global.GlobalConfig.BuildPath = buildPath
+
 	return &OTAPlugin{}
 }
 
